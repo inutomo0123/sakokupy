5c3a3f2282cf8b12651e5bf83a76af1d890cf184
inukai tomoki <inutomo0123@gmail.com>
2020-11-20 23:53:37 +0900

    third commit

--- sandbox/country/scrape.py
+++ sandbox/country/scrape.py
@@ -1,30 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 
-uri = 'https://ja.wikipedia.org/' \
-    + 'wiki/%E5%9B%BD%E5%90%8D%E3%82%B3%E3%83%BC%E3%83%89'
-
 uri = 'https://ja.wikipedia.org/wiki/ISO_3166-1'
 
 res = requests.get(uri)
 
 soup = BeautifulSoup(res.text, 'html.parser')
 
-print(soup.find('title').get_text())
-
-print(soup.select_one('title').get_text())
-
-print(soup.title.get_text())
-
-print(soup.title)
+#print(soup.find('title').get_text())
+#
+#print(soup.select_one('title').get_text())
+#
+#print(soup.title.get_text())
+#
+#print(soup.title)
 
 from pprint import pprint as pp
 
 #pp(soup.select('h2>span#略号一覧'))
 #pp(soup.select('h2:has( h2>span#略号一覧 )'))
 
-print(soup.select_one('h2>span#略号一覧').parent)
-print(soup.select_one('h2>span#略号一覧').parent.find_next_siblings())
+#print(soup.select_one('h2>span#略号一覧').parent)
+
+#print(soup.select_one('h2>span#略号一覧').parent.find_next_siblings())
 #print(soup.select_one('h2>span#略号一覧').parent.parent.select('table tbody tr'))
-print(soup.select_one('h2>span#略号一覧').parent.find_next_siblings()[0]\
-      .select_one("table"))
+
+#print(soup.select_one('h2>span#略号一覧').parent.find_next_siblings()[0]\
+#      .select("table tbody tr ")
+#      )
+
+tr = soup.select_one('h2>span#略号一覧').parent.find_next_sibling().find_next_sibling().find_next_sibling().select('table tbody tr')
+
+def row(td):
+
+    return [td[4].get_text(),
+            td[1].get_text(),
+            td[0].get_text().strip(),
+            td[5].get_text()]
+
+
+
+
+
+
+
+
+pp(list(map(lambda x:
+         row(x.select('td')),
+         list(filter(lambda tr_ch: len(tr_ch.select('td')) == 7, tr))
+         )))
+
+#print(list(filter(lambda tr_ch: len(tr_ch.select('td')) == 7, tr)))
